71836e0d0dc699d83ea493ba422d7981b7fea043
Abdoul19 <abdoul19@gmail.com>
2020-12-22 18:41:21 +0000

    Search option

--- src/app.controller.js
+++ src/app.controller.js
@@ -12,10 +12,51 @@ export class AppController {
 
   @Get()
   getHello() {
-    
-    //return 'Hello World';
+    const params = {
+      $from: "2019-08-03 11:22:47",
+      $to: "2020-08-03 11:22:47",
+      $sort: {
+          "created_at": "desc"
+      },
+      $perPage: 200,
+      $page: 1
+    }
+     //return 'Hello World';
     
   }
 
-  
+  createSortFilter(field, direction){
+    return {
+      field: field,
+      direction: direction
+    }
+  }
+
+  createRangeFilter(field, valueFrom, valueTo){
+    const from = {
+      field: field,
+      value: valueFrom,
+      condition_type: 'from',
+    };
+
+    const to = {
+      field: field,
+      value: valueTo,
+      condition_type: 'to',
+    };
+
+    return { from, to};
+  }
+
+
+  createFilter(field, value, conditionType){
+    return {
+      field: field.toString(),
+      value: value.toString(),
+      condition_type: conditionType.toString(),
+    }
+  }
+
+
+
 }
